e23cd5156f32da320e2043127d6411664d4b8ac3
Vivien Le Duc <leducvivien@gmail.com>
2020-11-17 13:10:52 +0100

    Updates

---
+++ precache-manifest.f0e2fdb12b9096519b1a44f10e3b9ef7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36efe02b452a6afbf586f2e2d2e9f925",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b751b95a10ef2eceeeaf",
+    "url": "./static/css/main.63a3795e.chunk.css"
+  },
+  {
+    "revision": "2740e380ce7e86396f79",
+    "url": "./static/js/2.96cb587d.chunk.js"
+  },
+  {
+    "revision": "b0a3abb66ec899db13c83cb08cd321c7",
+    "url": "./static/js/2.96cb587d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b751b95a10ef2eceeeaf",
+    "url": "./static/js/main.cd968507.chunk.js"
+  },
+  {
+    "revision": "e0a58147d809bfbed5f5",
+    "url": "./static/js/runtime-main.9adf4be3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b989cd23d826b8f9beb07ac3d7a69a0e.js"
+  "./precache-manifest.f0e2fdb12b9096519b1a44f10e3b9ef7.js"
 );
 
 self.addEventListener('message', (event) => {
