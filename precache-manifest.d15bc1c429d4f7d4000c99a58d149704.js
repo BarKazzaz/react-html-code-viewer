4705617a8d4dd4fbe60bd8ee87f15c0cda985b78
Vivien Le Duc <leducvivien@gmail.com>
2020-11-17 10:17:23 +0100

    Updates

---
+++ precache-manifest.d15bc1c429d4f7d4000c99a58d149704.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b582e64162e1cfc958b4d017e78e03e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ecf0b80546d0276cccbd",
+    "url": "./static/css/main.55120a99.chunk.css"
+  },
+  {
+    "revision": "2fae84c08983362f2046",
+    "url": "./static/js/2.f37df1af.chunk.js"
+  },
+  {
+    "revision": "b0a3abb66ec899db13c83cb08cd321c7",
+    "url": "./static/js/2.f37df1af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecf0b80546d0276cccbd",
+    "url": "./static/js/main.477af8e0.chunk.js"
+  },
+  {
+    "revision": "e0a58147d809bfbed5f5",
+    "url": "./static/js/runtime-main.9adf4be3.js"
+  }
+]);
