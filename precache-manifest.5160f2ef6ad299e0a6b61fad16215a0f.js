f54c79903889570d0e9e3312a4d488e2296765af
Vivien Le Duc <leducvivien@gmail.com>
2020-11-18 01:09:04 +0100

    Updates

--- precache-manifest.5160f2ef6ad299e0a6b61fad16215a0f.js
+++ precache-manifest.5160f2ef6ad299e0a6b61fad16215a0f.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3d2a4f4a190b6d2829f498011d87091c",
+    "revision": "2a553a61139b8e6d45949078b5e51552",
     "url": "./index.html"
   },
   {
-    "revision": "b6e181078355af90cd89",
-    "url": "./static/css/main.1df67201.chunk.css"
+    "revision": "01704cb1aea2cdcdb37b",
+    "url": "./static/css/main.08c8fb1b.chunk.css"
   },
   {
     "revision": "972a857ebb14364033f0",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.978ffc3b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b6e181078355af90cd89",
-    "url": "./static/js/main.960c48b6.chunk.js"
+    "revision": "01704cb1aea2cdcdb37b",
+    "url": "./static/js/main.ff9f0d27.chunk.js"
   },
   {
     "revision": "e0a58147d809bfbed5f5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5be5e7004b8cce1845c0779832af8d53.js"
+  "./precache-manifest.5160f2ef6ad299e0a6b61fad16215a0f.js"
 );
 
 self.addEventListener('message', (event) => {
